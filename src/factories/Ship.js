21574ccd6c979c0885beaedd2ebd2b87c912f8d2
Daxas <66393125+daxas-boop@users.noreply.github.com>
2020-10-28 00:59:12 -0300

    Removes redundant validation

--- src/factories/Ship.js
+++ src/factories/Ship.js
@@ -2,7 +2,6 @@ const shipFactory = (length) => {
     const shipLife = new Array(length).fill('o');
 
     const hit = (position) => {
-        if (position < 0 || typeof position === 'undefined') return;
         shipLife.splice(position, 1, 'hit');
     }
 
