26b818d56dea1ad8bb6f1d0a42559bec6d9644a2
daxas-boop <daxas.swd@hotmail.com>
2020-11-04 19:38:33 -0300

    Updates

---
+++ precache-manifest.907c47577b3c12006686a2d5da674c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecd73578f312e2371b6e54a9428f8c5",
+    "url": "/battleship-tdd/index.html"
+  },
+  {
+    "revision": "529f28f1b92fa92b3105",
+    "url": "/battleship-tdd/static/css/2.842e1da4.chunk.css"
+  },
+  {
+    "revision": "529f28f1b92fa92b3105",
+    "url": "/battleship-tdd/static/js/2.981582cc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/battleship-tdd/static/js/2.981582cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62fed3f8cc196ab210ca",
+    "url": "/battleship-tdd/static/js/main.21bede88.chunk.js"
+  },
+  {
+    "revision": "6faaf8c07f76d123cea4",
+    "url": "/battleship-tdd/static/js/runtime-main.cd6ac8f4.js"
+  }
+]);
