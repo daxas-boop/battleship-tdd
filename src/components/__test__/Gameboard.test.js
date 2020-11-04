d5a55446904514f6c12902fda3a55f3a84f43856
daxas-boop <daxas.swd@hotmail.com>
2020-11-03 22:27:47 -0300

    Adds Gameboard component tests

--- src/components/__test__/Gameboard.test.js
+++ src/components/__test__/Gameboard.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import Dialog from '../Dialog';
+import Gameboard from '../Gameboard';
+import { fireEvent, render, screen } from "@testing-library/react";
 
-test('renders correctly', () => {
-    const component = renderer.create(<Dialog></Dialog>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
+const mockCallback = jest.fn(x => x)
+
+const providerProps = {
+    humanGameboard: [
+        [{},{}],
+        ['sunked ship','x']
+    ],
+    aiGameboard:[
+        [0,{}],
+        ['sunked ship','x']
+    ],
+    shipsRemaining: {humanShips:5,aiShips:5},
+    cellOnClick: mockCallback
+}
+
+test('Cell on click calls cellOnClick', () => {
+    render(<Gameboard {...providerProps} />);
+
+    expect(screen.queryAllByTestId('cell')[0]).toBeInTheDocument();
+    fireEvent.click(screen.queryAllByTestId('cell')[0]);
+    expect(mockCallback.mock.calls.length).toBe(1);
+})
+
+test('Human gameboard renders correctly.', () => {
+
+})
+
+test('Ai gameboard renders correctly.', () => {
+
+})
